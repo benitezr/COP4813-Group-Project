775954c58402d7351737ebf826432456ac5fe6d0
Rich <n00783340@ospreys.unf.edu>
2016-12-04 21:16:59 -0500

    Link to multiple CSS files
    Can link to multiple CSS files by populating the $styles array in the
    header.php file

--- header.php
+++ header.php
@@ -4,5 +4,13 @@
         <meta charset="UTF-8">
         <title><?php echo $pageTitle; ?></title>
         <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <?php
+        //Populate the $styles array with paths to other CSS files
+            if(isset($styles)){
+                foreach($styles as $path){
+                    echo "<link rel='stylesheet' type='text/css' href='$path'>";
+                }
+            }
+        ?>
     </head>
     <body>

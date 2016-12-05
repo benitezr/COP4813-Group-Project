8c26d03f448abce16d817cf63d8e3554d143d594
Rich <n00783340@ospreys.unf.edu>
2016-12-05 02:38:56 -0500

    Empty comment is discarded

--- editComment_p.php
+++ editComment_p.php
@@ -7,6 +7,11 @@
     $categoryID = $_POST["categoryID"];
     $content = mysql_real_escape_string(htmlspecialchars($_POST["commentContent"]));
 
+    if(empty($content)){
+        header("Location: thread.php?tid=$threadID&cid=$categoryID");
+        die();
+    }
+
     $query = "UPDATE Comments SET commentContent='$content' WHERE commentID=$commentID";
     $result = mysql_query($query) or die(mysql_error());
     mysql_close($db_access);
